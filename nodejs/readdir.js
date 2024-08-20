9fb0182613794498b8cdf38a98ca29de14487732
hojji <shimn3060@naver.com>
2024-08-20 23:19:47 +0900

    feat: 최종본

--- nodejs/readdir.js
+++ nodejs/readdir.js
@@ -1,7 +1,6 @@
-var testFolder = 'data';
+var testFolder = './data';
 var fs = require('fs');
 
-fs.readdir(testFolder,function(error, filelist){
-    console.log(filelist);
+fs.readdir(testFolder, function(error, filelist){
+  console.log(filelist);
 })
-//폴더의 파일을 배열로 파일목록을 읽음
